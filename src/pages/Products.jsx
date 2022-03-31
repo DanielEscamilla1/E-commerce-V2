0795e008aefb908fc96e39bbcf037a29ba2caed1
Daniel Escamilla <descamilla128@gmail.com>
2022-03-31 00:04:32 -0400

    fixing errors

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,7 +7,7 @@ import ProductData from "../productlist/Productdata";
 
 export default class Products extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       products: [],
     };
@@ -20,25 +20,31 @@ export default class Products extends React.Component {
     });
   }
 
-
   sortFun = (e) => {
     e.preventDefault();
-    console.log(e.target.value)
-    const result = this.state.products.filter(product => product.price < e.target.value);
-    this.setState({ products: result })
+    console.log(e.target.value);
+    const result = this.state.products.filter(
+      (product) => product.price < e.target.value
+    );
+    this.setState({ products: result });
     console.log(this.state.products);
-  }
+  };
   render() {
     return (
       <div className="productPage">
         <h1 className="productsH1">Products</h1>
         {/* <Filter /> */}
         {/* <button onClick={this.sortFun}>Sort</button> */}
-        <select name="filter" id="priceSelect" onChange={this.sortFun}>
+        <select
+          className="filterSelector"
+          name="filter"
+          id="priceSelect"
+          onChange={this.sortFun}
+        >
+          <option value="10000">ALL</option>
           <option value="200">less than 200</option>
           <option value="500">less than 500</option>
           <option value="800">less than 800</option>
-          <option value="10000">ALL</option>
         </select>
         <main>
           <div className="cards">
